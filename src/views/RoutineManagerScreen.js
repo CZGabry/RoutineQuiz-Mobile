97787c9e3f2f1761a5ac25f4de8106db5b635918
GabrieleTatatu <gabriele.sacchet@tatatu.it>
2024-03-20 10:11:20 +0100

    Updated routine manager

--- src/views/RoutineManagerScreen.js
+++ src/views/RoutineManagerScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text, Button, TouchableOpacity, View, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -17,35 +17,72 @@ const daysOfWeek = [
 
 const RoutineManager = () => {
   const [selectedDays, setSelectedDays] = useState([]);
-  const [hours, setHours] = useState([new Date()]);
-  const [showPicker, setShowPicker] = useState([false]);
+  const [hours, setHours] = useState([]);
+  const [showPicker, setShowPicker] = useState([]);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
-  const handleDayToggle = (dayValue) => {
-  if (dayValue === 0) { // 'All'
-    if (selectedDays.length < daysOfWeek.length - 1) { // Exclude 'All'
-      setSelectedDays(daysOfWeek.slice(1).map(day => day.value)); // Exclude 'All' and map to values
-    } else {
-      setSelectedDays([]);
-    }
-  } else {
-    if (selectedDays.includes(dayValue)) {
-      setSelectedDays(selectedDays.filter(d => d !== dayValue));
-    } else {
-      const updatedDays = [...selectedDays, dayValue];
-      if (updatedDays.length === daysOfWeek.length - 1) { // Check against the length excluding 'All'
-        setSelectedDays(daysOfWeek.slice(1).map(day => day.value)); // Exclude 'All' and map to values
+
+  useEffect(() => {
+    setLoading(true);
+    AsyncStorage.getItem('userToken').then(userToken => {
+      if (userToken) {
+        axios.get('http://10.0.2.2:3000/api/getroutine', {
+          headers: { Authorization: `Bearer ${userToken}` },
+        })
+          .then(response => {
+            const { days, hours } = response.data.routine;
+            if (days.length > 0 && hours.length > 0) {
+              // Set selected days
+              setSelectedDays(days);
+  
+              // Convert string hours to Date objects
+              const routineHours = hours.map(timeString => {
+                const [hour, minute] = timeString.split(':');
+                const date = new Date();
+                date.setHours(+hour, +minute, 0);
+                return date;
+              });
+              setHours(routineHours);
+  
+              // Prepare showPicker state based on the hours
+              setShowPicker(routineHours.map(() => false));
+            } else {
+              // Handle case where there are no routines
+              setMessage('No routines found.');
+            }
+          })
+          .catch(error => {
+            console.error('Failed to fetch routines:', error);
+            setMessage('Failed to load routines. Please try again later.');
+          })
+          .finally(() => {
+            setLoading(false);
+          });
       } else {
-        setSelectedDays(updatedDays);
+        setMessage('User token not found. Please login again.');
+        setLoading(false);
       }
+    }).catch(error => {
+      console.error('AsyncStorage error:', error);
+      setMessage('An error occurred while accessing local storage. Please try again.');
+      setLoading(false);
+    });
+  }, []);
+  
+  
+
+  const handleDayToggle = (dayValue) => {
+    if (dayValue === 0) { // 'All'
+      setSelectedDays(selectedDays.length < daysOfWeek.length - 1 ? daysOfWeek.slice(1).map(day => day.value) : []);
+    } else {
+      setSelectedDays(selectedDays.includes(dayValue) ? selectedDays.filter(d => d !== dayValue) : [...selectedDays, dayValue].sort());
     }
-  }
-};
+  };
 
   const handleHourChange = (event, selectedDate, index) => {
-    const newHours = [...hours];
-    newHours[index] = selectedDate || newHours[index];
-    setHours(newHours);
+    const updatedHours = [...hours];
+    updatedHours[index] = selectedDate || hours[index];
+    setHours(updatedHours);
     togglePicker(index);
   };
 
@@ -60,89 +97,66 @@ const RoutineManager = () => {
     setShowPicker([...showPicker, false]);
   };
 
-  const submitRoutine = () => {
+  const submitRoutine = async () => {
     setLoading(true);
-    setMessage(''); // Reset message on new request
-
-    AsyncStorage.getItem('userToken').then((userToken) => {
+    setMessage('');
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
       if (userToken) {
-        console.log("selectedDays");
-    const formattedHours = hours.map(hour => hour.toLocaleTimeString());
-    console.log({ selectedDays, hours: formattedHours });
-    const payload = {
-      hours: hours,
-      selectedDays: selectedDays,
-      user_token: userToken,
-    };
-    axios.post('http://10.0.2.2:3000/api/setroutine', payload)
-          .then(response => {
-            console.log('Upload successful:', response.data);
-            setMessage('Routine generated');
-            // Optional: Navigate to another screen upon success
-            // navigation.navigate('HomeScreen');
-          })
-          .catch(error => {
-            console.log('Routine failed:', error);
-            setMessage(error.response?.data.error || 'Routine failed. Please try again.');
-          })
-          .finally(() => {
-            setLoading(false);
-          });
+        const payload = {
+          selectedDays,
+          hours: hours.map(hour => hour.toISOString()),
+          user_token: userToken,
+        };
+        const response = await axios.post('http://10.0.2.2:3000/api/setroutine', payload);
+        setMessage('Routine saved successfully');
       } else {
-        console.log('User token not found');
         setMessage('User token not found. Please login again.');
-        setLoading(false);
       }
-    }).catch(error => {
-      console.log('Error getting user token:', error);
-      setMessage('An error occurred. Please try again.');
+    } catch (error) {
+      setMessage('Failed to save routine. Please try again later.');
+    } finally {
       setLoading(false);
-    });
+    }
   };
 
   return (
     <ScrollView style={styles.container}>
-    <Text style={styles.title}>Select Days</Text>
-    <View style={styles.daysRow}>
-      {daysOfWeek.slice(0, 4).map(day => ( // Adjusted to slice from 1 to exclude 'All' and split the first four days
-        <TouchableOpacity key={day.value} onPress={() => handleDayToggle(day.value)} style={selectedDays.includes(day.value) ? styles.dayButtonSelected : styles.dayButton}>
-          <Text style={selectedDays.includes(day.value) ? styles.dayTextSelected : styles.dayText}>
-            {day.name}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={styles.daysRow}>
-      {daysOfWeek.slice(4).map(day => ( // Adjusted to slice from 5 to get the last three days
-        <TouchableOpacity key={day.value} onPress={() => handleDayToggle(day.value)} style={selectedDays.includes(day.value) ? styles.dayButtonSelected : styles.dayButton}>
-          <Text style={selectedDays.includes(day.value) ? styles.dayTextSelected : styles.dayText}>
-            {day.name}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-
-    <Text style={styles.title}>Select Hours</Text>
-    {hours.map((hour, index) => (
-      <View key={index} style={styles.hourRow}>
-        <TouchableOpacity onPress={() => togglePicker(index)} style={styles.hourButton}>
-          <Text>Select Hour: {hour.toLocaleTimeString()}</Text>
-        </TouchableOpacity>
-        {showPicker[index] && (
-          <DateTimePicker
-            value={hour}
-            mode="time"
-            is24Hour={true}
-            display="default"
-            onChange={(event, selectedDate) => handleHourChange(event, selectedDate, index)}
-          />
-        )}
+      <Text style={styles.title}>Select Days</Text>
+      <View style={styles.daysRow}>
+        {daysOfWeek.map(day => (
+          <TouchableOpacity
+            key={day.value}
+            onPress={() => handleDayToggle(day.value)}
+            style={selectedDays.includes(day.value) ? styles.dayButtonSelected : styles.dayButton}
+          >
+            <Text style={selectedDays.includes(day.value) ? styles.dayTextSelected : styles.dayText}>
+              {day.name}
+            </Text>
+          </TouchableOpacity>
+        ))}
       </View>
-    ))}
-    
-    <Button onPress={addHour} title="Add Hour" disabled={hours.length >= 10} />
-    <Button onPress={submitRoutine} title="Submit" />
-  </ScrollView>
+      <Text style={styles.title}>Select Hours</Text>
+      {hours.map((hour, index) => (
+        <View key={index} style={styles.hourRow}>
+          <TouchableOpacity onPress={() => togglePicker(index)} style={styles.hourButton}>
+            <Text>Select Hour: {hour.toLocaleTimeString()}</Text>
+          </TouchableOpacity>
+          {showPicker[index] && (
+            <DateTimePicker
+              value={hour}
+              mode="time"
+              is24Hour={true}
+              display="default"
+              onChange={(event, selectedDate) => handleHourChange(event, selectedDate, index)}
+            />
+          )}
+        </View>
+      ))}
+      <Button onPress={addHour} title="Add Hour" disabled={hours.length >= 10} />
+      <Button onPress={submitRoutine} title="Submit" disabled={loading} />
+      {message ? <Text>{message}</Text> : null}
+    </ScrollView>
   );
 };
 
@@ -157,6 +171,7 @@ const styles = StyleSheet.create({
   },
   daysRow: {
     flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'space-between',
     marginBottom: 5,
   },
@@ -165,6 +180,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: '#ccc',
+    marginBottom: 5,
   },
   dayButtonSelected: {
     padding: 10,
@@ -172,6 +188,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'blue',
     backgroundColor: '#e0f0ff',
+    marginBottom: 5,
   },
   dayText: {
     textAlign: 'center',
